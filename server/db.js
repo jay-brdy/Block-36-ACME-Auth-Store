deed8ce3d461555df73281207768b2e0105375e1
jay-brdy <jaysirit@gmail.com>
2024-03-05 02:52:45 -0600

    coding during class

--- server/db.js
+++ server/db.js
@@ -2,8 +2,11 @@ const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_auth_store_db');
 const uuid = require('uuid');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const JWT = process.env.JWT || 'shhh';
 
-const createTables = async()=> {
+
+const createTables = async () => {
   const SQL = `
     DROP TABLE IF EXISTS favorites;
     DROP TABLE IF EXISTS users;
@@ -27,7 +30,7 @@ const createTables = async()=> {
   await client.query(SQL);
 };
 
-const createUser = async({ username, password})=> {
+const createUser = async ({ username, password }) => {
   const SQL = `
     INSERT INTO users(id, username, password) VALUES($1, $2, $3) RETURNING *
   `;
@@ -35,7 +38,7 @@ const createUser = async({ username, password})=> {
   return response.rows[0];
 };
 
-const createProduct = async({ name })=> {
+const createProduct = async ({ name }) => {
   const SQL = `
     INSERT INTO products(id, name) VALUES($1, $2) RETURNING *
   `;
@@ -43,7 +46,7 @@ const createProduct = async({ name })=> {
   return response.rows[0];
 };
 
-const createFavorite = async({ user_id, product_id })=> {
+const createFavorite = async ({ user_id, product_id }) => {
   const SQL = `
     INSERT INTO favorites(id, user_id, product_id) VALUES($1, $2, $3) RETURNING *
   `;
@@ -51,32 +54,48 @@ const createFavorite = async({ user_id, product_id })=> {
   return response.rows[0];
 };
 
-const destroyFavorite = async({ user_id, id })=> {
+const destroyFavorite = async ({ user_id, id }) => {
   const SQL = `
     DELETE FROM favorites WHERE user_id=$1 AND id=$2
   `;
   await client.query(SQL, [user_id, id]);
 };
 
-const authenticate = async({ username, password })=> {
+const authenticate = async ({ username, password }) => {
   const SQL = `
-    SELECT id, username FROM users WHERE username=$1;
+    SELECT id, password, username 
+    FROM users 
+    WHERE username=$1;
   `;
   const response = await client.query(SQL, [username]);
-  if(!response.rows.length){
+  if (!response.rows.length || (await bcrypt.compare(password, response.rows[0].password)) === false) {
     const error = Error('not authorized');
     error.status = 401;
     throw error;
   }
+  const token = await jwt.sign({ id: response.rows[0].id }, JWT);
+  console.log(token);
   return { token: response.rows[0].id };
 };
 
-const findUserWithToken = async(id)=> {
+const findUserWithToken = async (token) => {
+  let id;
+  try {
+    const payload = await jwt.verify(token, JWT);
+    id = payload.id
+  } catch (ex) {
+    const error = Error('not authorized');
+    error.status = 401;
+    throw error;
+  }
+
   const SQL = `
-    SELECT id, username FROM users WHERE id=$1;
+    SELECT id, username 
+    FROM users 
+    WHERE id=$1;
   `;
   const response = await client.query(SQL, [id]);
-  if(!response.rows.length){
+  if (!response.rows.length) {
     const error = Error('not authorized');
     error.status = 401;
     throw error;
@@ -84,7 +103,7 @@ const findUserWithToken = async(id)=> {
   return response.rows[0];
 };
 
-const fetchUsers = async()=> {
+const fetchUsers = async () => {
   const SQL = `
     SELECT id, username FROM users;
   `;
@@ -92,7 +111,7 @@ const fetchUsers = async()=> {
   return response.rows;
 };
 
-const fetchProducts = async()=> {
+const fetchProducts = async () => {
   const SQL = `
     SELECT * FROM products;
   `;
@@ -100,7 +119,7 @@ const fetchProducts = async()=> {
   return response.rows;
 };
 
-const fetchFavorites = async(user_id)=> {
+const fetchFavorites = async (user_id) => {
   const SQL = `
     SELECT * FROM favorites where user_id = $1
   `;
--- server/index.js
+++ server/index.js
@@ -20,6 +20,15 @@ const path = require('path');
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../client/dist/index.html')));
 app.use('/assets', express.static(path.join(__dirname, '../client/dist/assets'))); 
 
+const isLoggedIn = async(req, res, next)=> {
+  try {
+    req.user = await findUserByToken(req.headers.authorization);
+    next();
+  }
+  catch(ex){
+    next(ex);
+  }
+};
 
 app.post('/api/auth/login', async(req, res, next)=> {
   try {
@@ -30,7 +39,7 @@ app.post('/api/auth/login', async(req, res, next)=> {
   }
 });
 
-app.get('/api/auth/me', async(req, res, next)=> {
+app.get('/api/auth/me', isLoggedIn, async(req, res, next)=> {
   try {
     res.send(await findUserWithToken(req.headers.authorization));
   }
@@ -48,7 +57,7 @@ app.get('/api/users', async(req, res, next)=> {
   }
 });
 
-app.get('/api/users/:id/favorites', async(req, res, next)=> {
+app.get('/api/users/:id/favorites', isLoggedIn, async(req, res, next)=> {
   try {
     res.send(await fetchFavorites(req.params.id));
   }
@@ -57,7 +66,7 @@ app.get('/api/users/:id/favorites', async(req, res, next)=> {
   }
 });
 
-app.post('/api/users/:id/favorites', async(req, res, next)=> {
+app.post('/api/users/:id/favorites', isLoggedIn, async(req, res, next)=> {
   try {
     res.status(201).send(await createFavorite({ user_id: req.params.id, product_id: req.body.product_id}));
   }
@@ -66,7 +75,7 @@ app.post('/api/users/:id/favorites', async(req, res, next)=> {
   }
 });
 
-app.delete('/api/users/:user_id/favorites/:id', async(req, res, next)=> {
+app.delete('/api/users/:user_id/favorites/:id', isLoggedIn, async(req, res, next)=> {
   try {
     await destroyFavorite({user_id: req.params.user_id, id: req.params.id });
     res.sendStatus(204);
